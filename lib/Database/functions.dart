75a981bc25de1393b84bf62b362cd4e15c94a255
mayurpoptani <mayurpoptani24@gmail.com>
2020-07-04 16:41:58 +0530

    Initial Commit

---
+++ lib/Database/functions.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:chat_app/Database/variables.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+const DATABASEFILENAME = "myDatabase.db";
+
+void initDatabase() async {
+  String dbFolderPath = await getDatabasesPath();
+  var path = join(dbFolderPath, DATABASEFILENAME);
+  if(!File(path).existsSync()) File(path).createSync(recursive: true);
+  db = await openDatabase(path,
+    version: 1,
+    onCreate: (db, version) => print("Database Created Version "+version.toString()),
+    onOpen: (db) => print("Database Opened"),
+    singleInstance: true,
+  );
+}
